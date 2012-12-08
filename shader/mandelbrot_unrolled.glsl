1ff8cef0c125116898287d516becf8c9951fa15c
Nicolas F <ovdev@fratti.ch>
2012-12-08 23:36:48 +0100

    Replaced log(2.0) with a constant value
    Didn't boost performance significantly. :(

--- shader/mandelbrot_unrolled.glsl
+++ shader/mandelbrot_unrolled.glsl
@@ -24,7 +24,7 @@ void main() {
 		z_r_q = z_real_2 * z_real_2;	// z_real^2
 		z_i_q = z_imag_2 * z_imag_2;	// z_imag^2
 		if(z_r_q + z_i_q > 4.0) {
-			break;		// Oi, LEG IT
+			break;
 		}
 		z_imag = 2.0 * z_real_2 * z_imag_2 + c_imag;
 		z_real = z_r_q - z_i_q + c_real;
@@ -32,11 +32,11 @@ void main() {
 		z_real_2 = z_real * z_real - z_imag * z_imag + c_real;
 	}
 	if(z_imag * z_imag + z_real * z_real > 4.0) {
-		--i;	// Blimey, we went too far!
+		--i;	// Step one iteration back
 
-		ni = float(i) + 1.0 - log(log(float(sqrt(z_imag * z_imag + z_real * z_real)))) / log(2.0);
+		ni = float(i) + 1.0 - log(log(float(sqrt(z_imag * z_imag + z_real * z_real)))) / 0.69315; // 0.69315 = log(2.0)
 	} else {
-		ni = float(i) + 1.0 - log(log(float(sqrt(z_r_q + z_i_q)))) / log(2.0);
+		ni = float(i) + 1.0 - log(log(float(sqrt(z_r_q + z_i_q)))) / 0.69315; // 0.69315 = log(2.0)
 	}
 	if(i >= iter) {	// Check whether value was inside the set
 		fragColor = vec4(0.0, 0.0, 0.0, 1.0);
